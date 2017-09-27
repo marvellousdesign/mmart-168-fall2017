10de6506888cc2c12a915b69547ddcdb834e9fcf
Mary Luong <marvellousdesign@users.noreply.github.com>
2017-09-26 19:06:49 -0700

    Homework 4 updates

--- hw04/scripts/part2.js
+++ hw04/scripts/part2.js
@@ -2,66 +2,71 @@ let language = 'English'
 let languageCode = 'en'
 let jsonData
 
+const setLanguage = code => {
+  //Note: language codes here: https://www.w3schools.com/tags/ref_language_codes.asp
+  languageCode = code
+  if (code === 'ru') {
+    language = 'Russian'
+  } else if (code === 'es') {
+    language = 'Spanish'
+  } else {
+    language = 'English'
+  }
+  document.getElementById('language').innerHTML = language
 
-const setLanguage = (code) => {
-    //Note: language codes here: https://www.w3schools.com/tags/ref_language_codes.asp
-    languageCode = code
-    if (code === 'ru') {
-        language = 'Russian'
-    } else if (code === 'es') {
-        language = 'Spanish'
-    } else {
-        language = 'English'
-    }
-    document.getElementById('language').innerHTML = language
-
-    //clear form:
-    document.querySelector("pre").innerHTML = ''
+  //clear form:
+  document.querySelector('pre').innerHTML = ''
 }
 
-
+/* Step 2:
+  1. getData has several function with in it, whatever term you inputted will be fetched and display per your specfic langauge settings.
+  2. jsonData.statuses[0]
+  3. jsonData.statuses[4].text
+  4. jsonData.statuses[1].user.name
+*/
 const getData = () => {
-    const term = document.getElementById('term').value
-    const baseURL = 'https://mmart168-twitter-proxy.herokuapp.com/1.1/search/tweets.json?q=' + term
-    twitterURL = baseURL + '&lang=' + languageCode
-    fetch(twitterURL)
-        .then(function(response) {
-            return response.json()
-        })
-        .then(function(json) {
-            console.log(json)
-            jsonData = json
-            let formattedJSON = syntaxHighlight(JSON.stringify(json, null, "    "))
-            document.querySelector("pre").innerHTML = formattedJSON
-        })
+  const term = document.getElementById('term').value
+  const baseURL =
+    'https://mmart168-twitter-proxy.herokuapp.com/1.1/search/tweets.json?q=' +
+    term
+  twitterURL = baseURL + '&lang=' + languageCode
+  fetch(twitterURL)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(json) {
+      console.log(json)
+      jsonData = json
+      let formattedJSON = syntaxHighlight(JSON.stringify(json, null, '    '))
+      document.querySelector('pre').innerHTML = formattedJSON
+    })
 }
-
-
-
-
-
-
-
 
 /**********************************************************************/
 /* IGNORE THE CODE BELOW: JUST A WAY TO ADD COLOR TO THE JSON RESULTS */
 /**********************************************************************/
-const syntaxHighlight = (json) => {
-    // This is just a convenience function to add color to the JSON response:
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number'
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key'
-            } else {
-                cls = 'string'
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean'
-        } else if (/null/.test(match)) {
-            cls = 'null'
+const syntaxHighlight = json => {
+  // This is just a convenience function to add color to the JSON response:
+  json = json
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+  return json.replace(
+    /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+    function(match) {
+      var cls = 'number'
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
+          cls = 'key'
+        } else {
+          cls = 'string'
         }
-        return '<span class="' + cls + '">' + match + '</span>'
-    })
+      } else if (/true|false/.test(match)) {
+        cls = 'boolean'
+      } else if (/null/.test(match)) {
+        cls = 'null'
+      }
+      return '<span class="' + cls + '">' + match + '</span>'
+    }
+  )
 }
