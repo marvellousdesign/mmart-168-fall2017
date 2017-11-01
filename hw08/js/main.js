6d27293aa84a90c9e676462444e143b2c98c87d6
Mary Luong <marvellousdesign@users.noreply.github.com>
2017-10-31 22:23:05 -0700

    Completed homework 8

--- hw08/js/main.js
+++ hw08/js/main.js
@@ -12,49 +12,42 @@ const appendListElement = (text) => {
   document.querySelector('#part1').appendChild(li)
 }
 
-appendListElement('Welcome, ' + name + '! Make yourself comfortable.')
+let i = 0
+while ( i < 100) {
+  appendListElement('Welcome, ' + name + '! Make yourself comfortable.')
+  i++
+}
 
 //------------------------------------------------------------------
 // PART II: Using a for loop, write out each person’s avatar, name, and score, based on the people array
 //------------------------------------------------------------------
 //The data:
 var people = [
-    { name: 'Jane', pic: 'http://knight.gamebanana.com/img/ico/sprays/patrick_star_preview_2.png', score: 300 },
-    { name: 'Brenda', pic: 'https://3.bp.blogspot.com/-_3f5QzVwocE/U3G4_PeOoTI/AAAAAAAAeC0/uanC3ua1cu4/s1600/OldSpongeBobStock5-25-13.png', score: 10} ,
-     { name: 'Wanda', pic: 'https://3.bp.blogspot.com/-_3f5QzVwocE/U3G4_PeOoTI/AAAAAAAAeC0/uanC3ua1cu4/s1600/OldSpongeBobStock5-25-13.png', score: 60 },
-     { name: 'Maria', pic: 'http://knight.gamebanana.com/img/ico/sprays/patrick_star_preview_2.png', score: 80 },
-     { name: 'Jasper', pic: 'https://3.bp.blogspot.com/-_3f5QzVwocE/U3G4_PeOoTI/AAAAAAAAeC0/uanC3ua1cu4/s1600/OldSpongeBobStock5-25-13.png', score: 600 },
-     { name: 'Malik', pic: 'http://knight.gamebanana.com/img/ico/sprays/patrick_star_preview_2.png', score: 40 }
+  { name: 'Jane', pic: 'http://knight.gamebanana.com/img/ico/sprays/patrick_star_preview_2.png', score: 300 },
+  { name: 'Brenda', pic: 'https://3.bp.blogspot.com/-_3f5QzVwocE/U3G4_PeOoTI/AAAAAAAAeC0/uanC3ua1cu4/s1600/OldSpongeBobStock5-25-13.png', score: 10} ,
+  { name: 'Wanda', pic: 'https://3.bp.blogspot.com/-_3f5QzVwocE/U3G4_PeOoTI/AAAAAAAAeC0/uanC3ua1cu4/s1600/OldSpongeBobStock5-25-13.png', score: 60 },
+  { name: 'Maria', pic: 'http://knight.gamebanana.com/img/ico/sprays/patrick_star_preview_2.png', score: 80 },
+  { name: 'Jasper', pic: 'https://3.bp.blogspot.com/-_3f5QzVwocE/U3G4_PeOoTI/AAAAAAAAeC0/uanC3ua1cu4/s1600/OldSpongeBobStock5-25-13.png', score: 600 },
+  { name: 'Malik', pic: 'http://knight.gamebanana.com/img/ico/sprays/patrick_star_preview_2.png', score: 40 }
 ]
 
 
 // use a for loop to DRY up the printNames function
 // it should loop over the people array and append
 // the users avatar pic, a greeting, and their score to the HTML
-const printNames = () => {
-  // Message 1
-  const img1 = document.createElement('img')
-  img1.src = people[0].pic
-  img1.classList.add('avatar')
+const printNames = (arr) => {
+  const img = document.createElement('img')
+  img.src = people[arr].pic
+  img.classList.add('avatar')
 
-  const paragraph1 = document.createElement('p')
-  const text1 = document.createTextNode('Welcome, ' + people[0].name + '! Your score is: ' + people[0].score)
-  paragraph1.appendChild(text1)
+  const paragraph = document.createElement('p')
+  const text = document.createTextNode('Welcome, ' + people[arr].name + '! Your score is: ' + people[arr].score)
+  paragraph.appendChild(text)
 
-  document.querySelector('#part2').appendChild(img1)
-  document.querySelector('#part2').appendChild(paragraph1)
-
-  // Message 2
-  const img2 = document.createElement('img')
-  img2.src = people[1].pic
-  img2.classList.add('avatar')
-
-  const paragraph2 = document.createElement('p')
-  const text2 = document.createTextNode('Welcome, ' + people[1].name + '! Your score is: ' + people[1].score)
-  paragraph2.appendChild(text2)
-
-  document.querySelector('#part2').appendChild(img2)
-  document.querySelector('#part2').appendChild(paragraph2)
+  document.querySelector('#part2').appendChild(img)
+  document.querySelector('#part2').appendChild(paragraph)
 }
 
-printNames()
+for (let i = 0; i < people.length; i++) {
+  printNames(i)
+}
