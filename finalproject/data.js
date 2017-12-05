d554b5646ed38a11b665f78a4ccff0e29dd0b711
Mary Luong <marvellousdesign@users.noreply.github.com>
2017-12-05 00:20:59 -0800

    final project wip update2

---
+++ finalproject/data.js
@@ -0,0 +1,488 @@
+var PAGES = [
+	{
+		img: 'img/npc.jpg',
+		keywords: ['New', 'Game'],
+		text: 'Welcome to Choose Your Own Adventure!',
+		target: 0,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Continue',
+				target: 1
+			},
+			{
+				text: 'New Game',
+				target: 2
+			}
+		]
+	},
+	{
+		text: 'Continue',
+		keywords: ['Continue', 'Game'],
+		target: 3,
+		type: 'choice',
+		choices: [
+			{
+				text: 'World, Dungeon Save',
+				target: 4
+			}
+		]
+	},
+	{
+		text: 'Character Creation',
+		keywords: ['Character', 'Creation'],
+		target: 2,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Starting Game',
+				target: 3
+			}
+		]
+	},
+	{
+		text: 'You\'ve been mysterously transported to a island unbeknownst to you, and you have lost your memories.',
+		keywords: ['Ship reck', 'island'],
+		target: 3,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Explore World',
+				target: 5
+			}
+		]
+	},
+	{
+		text: 'World, Dungeon Save',
+		keywords: ['Explore'],
+		target: 4,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Dungeons',
+				target: 6
+			},
+			{
+				text: 'Explore World',
+				target: 5
+			}
+		]
+	},
+	{
+		img: 'img/world.jpg',
+		text: 'Explore World',
+		keywords: ['Explore', 'world'],
+		target: 5,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Towns',
+				target: 11
+			},
+			{
+				text: 'Castles',
+				target: 15
+			},
+			{
+				text: 'Towers',
+				target: 16
+			},
+			{
+				text: 'Dungeons',
+				target: 6
+			},
+			{
+				text: 'Enemies, Monsters',
+				target: 7
+			}
+		]
+	},
+	{
+		text: 'Dungeons',
+		keywords: ['dungoen'],
+		target: 6,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Explore',
+				target: 20
+			}
+		]
+	},
+	{
+		text: 'Enemies, Monsters',
+		keywords: ['monsters'],
+		target: 7,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Fight|Win',
+				target: 12
+			},
+			{
+				text: 'Lose|Towns',
+				target: 21
+			}
+		]
+	},
+	{
+		text: 'Loot, Treasures',
+		keywords: ['gold', 'money'],
+		target: 8,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Dungeons',
+				target: 6
+			},
+			{
+				text: 'Explore World',
+				target: 5
+			}
+		]
+	},
+	{
+		text: 'Mid Boss, Boss',
+		keywords: ['mid boss'],
+		target: 9,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Win',
+				target: 12
+			},
+			{
+				text: 'Defeat, Bad Ending Scene',
+				target: 10
+			}
+		]
+	},
+	{
+		text: 'Defeat, Bad Ending Scene',
+		keywords: ['game over'],
+		target: 10,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Game Over|Title Menu',
+				target: 0
+			}
+		]
+	},
+	{
+		img: 'img/town.jpg',
+		keywords: ['Towns'],
+		text: 'Towns',
+		target: 11,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Explore World',
+				target: 5
+			},
+			{
+				text: 'Pub, Inn',
+				target: 21
+			},
+			{
+				text: 'Markets',
+				target: 19
+			}
+		]
+	},
+	{
+		text: 'Win',
+		keywords: ['winner'],
+		target: 12,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Loot, Treasures',
+				target: 8
+			},
+			{
+				text: 'Explore',
+				target: 20
+			}
+		]
+	},
+	{
+		text: 'Level 2',
+		keywords: ['dungoen'],
+		target: 13,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Enemies, Monsters',
+				target: 7
+			},
+			{
+				text: 'Loot, Treasures',
+				target: 8
+			},
+			{
+				text: 'Level 3, etc',
+				target: 14
+			}
+		]
+	},
+	{
+		text: 'Level 3, etc',
+		keywords: ['dungoen'],
+		target: 14,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Enemies, Monsters',
+				target: 7
+			},
+			{
+				text: 'Loot, Treasures',
+				target: 8
+			},
+			{
+				text: 'Dramatic Boss Cutscene',
+				target: 17
+			}
+		]
+	},
+	{
+		img: 'img/castle.jpg',
+		keywords: ['castle'],
+		text: 'Castles',
+		target: 15,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Explore World',
+				target: 5
+			},
+			{
+				text: 'NPC Quests',
+				target: 18
+			}
+		]
+	},
+	{
+		text: 'Towers',
+		keywords: ['mid evil', 'tower'],
+		target: 16,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Explore',
+				target: 20
+			}
+		]
+	},
+	{
+		text: 'Dramatic Boss Cutscene',
+		keywords: ['cutscene', 'boss'],
+		target: 17,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Mid Boss, Boss',
+				target: 9
+			}
+		]
+	},
+	{
+		img: 'img/npcQuests.jpg',
+		text: 'Meow seeks you to rid the world of monsters seeking havok on the world',
+		target: 18,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Explore World',
+				target: 5
+			},
+			{
+				text: 'Castles',
+				target: 15
+			},
+			{
+				text: 'Towns',
+				target: 11
+			},
+			{
+				text: 'Pub, Inn',
+				target: 21
+			},
+			{
+				text: 'Markets',
+				target: 19
+			}
+		]
+	},
+	{
+		text: 'Markets',
+		keywords: ['item shops', 'midevil'],
+		target: 19,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Towns',
+				target: 11
+			},
+			{
+				text: 'NPC Quests',
+				target: 18
+			}
+		]
+	},
+	{
+		text: 'Explore',
+		keywords: ['Explore'],
+		target: 20,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Enemies, Monsters',
+				target: 7
+			},
+			{
+				text: 'Loot, Treasures',
+				target: 8
+			},
+			{
+				text: 'Level 2',
+				target: 13
+			},
+			{
+				text: 'Explore World',
+				target: 5
+			}
+		]
+	},
+	{
+		img: 'img/npc.jpg',
+		text: 'Welcome! How may I help you?',
+		target: 21,
+		type: 'choice',
+		choices: [
+			{
+				text: 'Towns',
+				target: 11
+			},
+			{
+				text: 'Talk',
+				target: 22
+			},
+			{
+				text: 'NPC Quests',
+				target: 18
+			}
+		]
+	},
+	{
+		img: 'img/npc.jpg',
+		target: 22,
+		text: 'From whence do you come, young one?',
+		type: 'choice',
+		choices: [
+			{
+				text: 'I am a traveler exploring the world.',
+				target: 23
+			},
+			{
+				text: 'I come from a farm on the outskirts of town.',
+				target: 24
+			},
+			{
+				text: 'I hail from a wealthy family. My father is a renowned merchant.',
+				target: 25
+			},
+			{
+				text: "You don't need to know that.",
+				target: 26
+			},
+			{
+				text: "I don't remember who I am.",
+				target: 27
+			}
+
+		]
+	},
+	{
+		img: 'img/npc.jpg',
+		target: 23,
+		text: 'Ah, a traveler? Fascinating!',
+		type: 'choice',
+		choices: [
+			{
+				text: 'Yes. Ask me that again.',
+				target: 22
+			},
+			{
+				text: 'Back',
+				target: 21
+			}
+		]
+	},
+	{
+		img: 'img/npc.jpg',
+		target: 24,
+		text: 'Ah, the rural life on the farm?',
+		type: 'choice',
+		choices: [
+			{
+				text: 'Yes. Ask me that again.',
+				target: 22
+			},
+			{
+				text: 'Back',
+				target: 21
+			}
+		]
+	},
+	{
+		img: 'img/npc.jpg',
+		target: 25,
+		text:
+			"A merchant's child? You're quite the young aristocrat, aren't you?",
+		type: 'choice',
+		choices: [
+			{
+				text: 'Yes. Ask me that again.',
+				target: 22
+			},
+			{
+				text: 'Back',
+				target: 21
+			}
+		]
+	},
+	{
+		img: 'img/npc.jpg',
+		target: 26,
+		text: "Ah, a shady type. Fine, you don't have to tell me.",
+		type: 'choice',
+		choices: [
+			{
+				text: 'Good. Ask me that again.',
+				target: 22
+			},
+			{
+				text: 'Back',
+				target: 21
+			}
+		]
+	},
+	{
+		img: 'img/npc.jpg',
+		target: 27,
+		text: "Ah, well that's a shame and I hope you remember in time.",
+		type: 'choice',
+		choices: [
+			{
+				text: 'Good. Ask me that again.',
+				target: 22
+			},
+			{
+				text: 'Back',
+				target: 21
+			}
+		]
+	}
+]
--- finalproject/main.js
+++ finalproject/main.js
@@ -1,533 +1,74 @@
+const key = 'AIzaSyA0OwYNa1ng9-5MAUhOpwOc9f2PU_kqeeY'
+const searchEngineID = '011931807795810912304:yctplun7xr4'
+
 // org https://jsfiddle.net/crmardix/zjWaj/
-// Page data
-var PAGES = [
-	{
-		img: 'img/npc.jpg',
-		text: 'Welcome!',
-		target: 0,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Continue',
-				link: 'Continue',
-				target: 1
-			},
-			{
-				text: 'New Game',
-				link: 'New Game',
-				target: 2
-			}
-		]
-	},
-	{
-		text: 'Continue',
-		target: 3,
-		type: 'choice',
-		choices: [
-			{
-				text: 'World, Dungeon Save',
-				link: 'World, Dungeon Save',
-				target: 4
-			}
-		]
-	},
-	{
-		text: 'Character Creation',
-		target: 2,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Starting Cave',
-				link: 'Character Creation',
-				target: 3
-			}
-		]
-	},
-	{
-		text: 'Starting Cave',
-		target: 3,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Explore World',
-				link: 'Explore World',
-				target: 5
-			}
-		]
-	},
-	{
-		text: 'World, Dungeon Save',
-		target: 4,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Dungeons',
-				link: 'Dungeons',
-				target: 6
-			},
-			{
-				text: 'Explore World',
-				link: 'Explore World',
-				target: 5
-			}
-		]
-	},
-	{
-        img: 'img/world.jpg',
-		text: 'Explore World',
-		target: 5,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Towns',
-				link: 'Towns',
-				target: 11
-			},
-			{
-				text: 'Castles',
-				link: 'Castles',
-				target: 15
-			},
-			{
-				text: 'Towers',
-				link: 'Towers',
-				target: 16
-			},
-			{
-				text: 'Dungeons',
-				link: 'Dungeons',
-				target: 6
-			},
-			{
-				text: 'Enemies, Monsters',
-				link: 'Enemies, Monsters',
-				target: 7
-			}
-		]
-	},
-	{
-		text: 'Dungeons',
-		target: 6,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Explore',
-				link: 'Explore',
-				target: 20
-			}
-		]
-	},
-	{
-		text: 'Enemies, Monsters',
-		target: 7,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Fight|Win',
-				link: 'Fight|Win',
-				target: 12
-			},
-			{
-				text: 'Lose|Towns',
-				link: 'Lose|Towns',
-				target: 21
-			}
-		]
-	},
-	{
-		text: 'Loot, Treasures',
-		target: 8,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Dungeons',
-				link: 'Dungeons',
-				target: 6
-			},
-			{
-				text: 'Explore World',
-				link: 'Explore World',
-				target: 5
-			}
-		]
-	},
-	{
-		text: 'Mid Boss, Boss',
-		target: 9,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Win',
-				link: 'Win',
-				target: 12
-			},
-			{
-				text: 'Defeat, Bad Ending Scene',
-				link: 'Defeat, Bad Ending Scene',
-				target: 10
-			}
-		]
-	},
-	{
-		text: 'Defeat, Bad Ending Scene',
-		target: 10,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Game Over|Title Menu',
-				link: 'Game Over|Title Menu',
-                target: 0
-			}
-		]
-	},
-	{
-        img: 'img/town.jpg',
-		text: 'Towns',
-		target: 11,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Explore World',
-				link: 'Explore World',
-				target: 5
-			},
-			{
-				text: 'Pub, Inn',
-				link: 'Pub, Inn',
-				target: 21
-			},
-			{
-				text: 'Markets',
-				link: 'Markets',
-				target: 19
-			}
-		]
-	},
-	{
-		text: 'Win',
-		target: 12,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Loot, Treasures',
-				link: 'Loot, Treasures',
-				target: 8
-			},
-			{
-				text: 'Explore',
-				link: 'Explore',
-				target: 20
-			}
-		]
-	},
-	{
-		text: 'Level 2',
-		target: 13,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Enemies, Monsters',
-				link: 'Enemies, Monsters',
-				target: 7
-			},
-			{
-				text: 'Loot, Treasures',
-				link: 'Loot, Treasures',
-				target: 8
-			},
-			{
-				text: 'Level 3, etc',
-				link: 'Level 3, etc',
-				target: 14
-			}
-		]
-	},
-	{
-		text: 'Level 3, etc',
-		target: 14,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Enemies, Monsters',
-				link: 'Enemies, Monsters',
-				target: 7
-			},
-			{
-				text: 'Loot, Treasures',
-				link: 'Loot, Treasures',
-				target: 8
-			},
-			{
-				text: 'Dramatic Boss Cutscene',
-				link: 'Dramatic Boss Cutscene',
-				target: 17
-			}
-		]
-	},
-	{
-        img: 'img/castle.jpg',
-		text: 'Castles',
-		target: 15,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Explore World',
-				link: 'Explore World',
-				target: 5
-			},
-			{
-				text: 'NPC Quests',
-				link: 'NPC Quests',
-				target: 18
-			}
-		]
-	},
-	{
-		text: 'Towers',
-		target: 16,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Explore',
-				link: 'Explore',
-				target: 20
-			}
-		]
-	},
-	{
-		text: 'Dramatic Boss Cutscene',
-		target: 17,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Mid Boss, Boss',
-				link: 'Mid Boss, Boss',
-				target: 9
-			}
-		]
-	},
-	{
-        img: 'img/npcQuests.jpg',
-		text: 'NPC Quests',
-		target: 18,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Explore World',
-				link: 'Explore World',
-				target: 5
-			},
-			{
-				text: 'Castles',
-				link: 'Castles',
-				target: 15
-			},
-			{
-				text: 'Towns',
-				link: 'Towns',
-				target: 11
-			},
-			{
-				text: 'Pub, Inn',
-				link: 'Pub, Inn',
-				target: 21
-			},
-			{
-				text: 'Markets',
-				link: 'Markets',
-				target: 19
-			}
-		]
-	},
-	{
-		text: 'Markets',
-		target: 19,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Towns',
-				link: 'Towns',
-				target: 11
-			},
-			{
-				text: 'NPC Quests',
-				link: 'NPC Quests',
-				target: 18
-			}
-		]
-	},
-	{
-		text: 'Explore',
-		target: 20,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Enemies, Monsters',
-				link: 'Enemies, Monsters',
-				target: 7
-			},
-			{
-				text: 'Loot, Treasures',
-				link: 'Loot, Treasures',
-				target: 8
-			},
-			{
-				text: 'Level 2',
-				link: 'Level 2',
-				target: 13
-			},
-			{
-				text: 'Explore World',
-				link: 'Explore World',
-				target: 5
-			}
-		]
-	},
-	{
-        img: 'img/npc.jpg',
-		text: 'Welcome!',
-		target: 21,
-		type: 'choice',
-		choices: [
-			{
-				text: 'Towns',
-				link: 'Towns',
-				target: 11
-			},
-            {
-                text: 'Talk',
-                target: 22
-            },
-			{
-				text: 'NPC Quests',
-				link: 'NPC Quests',
-				target: 18
-			}
-		]
-
-	},
-    {
-        'img': 'img/npc.jpg',
-        target: 22,
-        'text': 'From whence do you come, young one?',
-            'type': 'choice',
-            'choices': [{
-            'text': 'I am a traveler exploring the world.',
-                'target': 23
-        }, {
-            'text': 'I come from a farm on the outskirts of town.',
-                'target': 24
-        }, {
-            'text': 'I hail from a wealthy family. My father is a renowned merchant.',
-                'target': 25
-        }, {
-            'text': 'You don\'t need to know that.',
-                'target': 26
-        }]
-    },
-    {
-       'img': 'img/npc.jpg',
-       target: 23,
-       'text': 'Ah, a traveler? Fascinating!',
-           'type': 'choice',
-           'choices': [{
-           'text': 'Yes. Ask me that again.',
-               'target': 22
-       }, {
-       'text': 'Back',
-           'target': 21
-   }]
-   }, {
-       'img': 'img/npc.jpg',
-       target: 24,
-       'text': 'Ah, the rural life on the farm?',
-           'type': 'choice',
-           'choices': [{
-           'text': 'Yes. Ask me that again.',
-               'target': 22
-       }, {
-       'text': 'Back',
-           'target': 21
-   }]
-   }, {
-       'img': 'img/npc.jpg',
-       target: 25,
-       'text': 'A merchant\'s child? You\'re quite the young aristocrat, aren\'t you?',
-           'type': 'choice',
-           'choices': [{
-           'text': 'Yes. Ask me that again.',
-               'target': 22
-       }, {
-       'text': 'Back',
-           'target': 21
-   }]
-   }, {
-       'img': 'img/npc.jpg',
-       target: 26,
-       'text': 'Ah, a shady type. Fine, you don\'t have to tell me.',
-           'type': 'choice',
-           'choices': [{
-           'text': 'Good. Ask me that again.',
-               'target': 22
-       }, {
-       'text': 'Back',
-           'target': 21
-   }]
-   }
-]
-
-const clear_page = () => {
-	document.getElementById('page_img').innerHTML = ''
-	document.getElementById('page_text').innerHTML = ''
+const clearPage = () => {
+	document.getElementById('pageImg').innerHTML = ''
+	document.getElementById('pageText').innerHTML = ''
 	document.getElementById('response').innerHTML = ''
 }
 
-const set_page_text = (text) => {
-	document.getElementById('page_text').innerHTML = `<p> ${text} </p>`
+const setPageText = (text) => {
+	document.getElementById('pageText').innerHTML = `<p> ${text} </p>`
 }
 
-const set_page_img = (img) => {
-	// const img = document.createElement('img')
-	// img.src = PAGES[arr].pic
-	// img.classList.add('avatar')
-	document.getElementById('page_img').innerHTML += `<img src=${img} />`
+const setPageImg = (img) => {
+	const keywords = PAGES.keywords
+	const googleImage = 'https://www.googleapis.com/customsearch/v1?key='
+	+ key + '&cx=' + searchEngineID + '&q=' + keywords + '&num=1&safe=medium&searchType=image'
+	const fetchImg = () => {
+		fetch(googleImage)
+		    .then(function(response) {
+		    return response.json();
+		}).then(function(json) {
+		    console.log(json)
+		    const items = json.items
+			items.forEach((item) => {
+		        console.log(item.link)
+		        const img = document.createElement('img')
+		        img.src = item.link
+				document.getElementById('pageImg').appendChild(img)
+		    })
+		});
+	}
+
+	if (keywords === undefined) {
+		document.getElementById('pageImg').innerHTML += `<img src=${img} />`
+	} else if (img === undefine) {
+		fetchImg()
+	} else {
+		fetchImg()
+	}
 }
 
-const add_choice = (text, target) => {
+const addChoice = (text, target) => {
 	document.getElementById('response').innerHTML +=
         `<button class=choice data-target= ${target} onClick="toggle(this)"> ${text} </button>`
 }
 
-const load_page = (id) => {
-	// Fetch JSON for page data associated with given ID
-	const page_data = PAGES[id]
-	console.log(PAGES[id])
+const loadPage = (target) => {
+	// Fetch JSON for page data associated with given data-target
+	const data = PAGES[target]
+	console.log('Current Page Keyword: ' + PAGES[target].keywords + ', Current Page: ' + PAGES[target])
 
-	clear_page()
-	set_page_text(page_data.text)
-	set_page_img(page_data.img)
-	console.log(page_data.text)
-	if (page_data.type === 'choice') {
-		for (let choice in page_data.choices) {
-			const choice_data = page_data.choices[choice]
-			add_choice(choice_data.text, choice_data.target)
+	clearPage()
+	setPageText(data.text)
+	setPageImg(data.img)
+	console.log('Page Text: ' + data.text + ', Keywords: ' + data.keywords + ', Img: ' + data.img)
+
+	if (data.type === 'choice') {
+		for (let choice in data.choices) {
+			const CYOA = data.choices[choice]
+			addChoice(CYOA.text, CYOA.target)
 		}
 	}
 }
 
-let current_page = 0
-
-//document.addEventListener('DOMContentLoaded', () => {
-load_page(0)
-//let response = document.getElementById('response')
-//document.querySelectorAll('.choice').forEach((choice) => {
+let currentPage = 0
+loadPage(0)
 const toggle = choice => {
 	choice.classList.toggle('choice')
-	//choice.addEventListener('click', () => {
 	const target = choice.getAttribute('data-target')
-	//var target = $(this).data('target')
-	load_page(target)
-	console.log(target)
-	//}, false);
+	loadPage(target)
+	console.log('Selected Target: ' + target)
 }
-//})
